b4bd6931cc3f765731ec2524a3461e13c1cc4673
dfyz85 <492587@gmail.com>
2010-09-28 15:04:30 +0300

    fgf

---
+++ my_downloud_img.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+'''
+Created on 27.09.2010
+
+@author: i.saltykov
+'''
+import urllib2
+import re
+import urllib
+from BeautifulSoup  import BeautifulSoup
+
+def downloud(url):
+    soup = BeautifulSoup(urllib2.urlopen(url))
+    tt = []
+    for i in soup.findAll('img'):
+        try:
+            if i['src']:
+                tt.append(i['src'])
+        except KeyError:
+            print 'sory'
+    tt = [i for i in list(set(tt)) if 'http' in i] 
+    #list(set(tt)) убирает все повторяющиеся записи в списки
+    
+        
+        
